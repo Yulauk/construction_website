afd7b6d9a0ce9c1327866c0f643a9f3df9672d31
yulauk <yulauk@gmail.com>
2023-12-14 01:45:07 +0100

    created a basic template for site

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, render_template, make_response
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def base():
+    return render_template('about.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("error_pages/404.html"), 404
+
+
+# url_for('static', filename='style.css')
