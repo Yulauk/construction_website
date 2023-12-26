465bc4b339e3cc32d38b648ac5a0eb1f80d8f3ff
yulauk <yulauk@gmail.com>
2023-12-26 15:09:26 +0100

    added a function that automatically changes the year in the footer and a feedback form has been created

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from flask import Flask, url_for, render_template, make_response
+import datetime
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', year_on_site=year_on_site())
+
 
 @app.route('/about')
 def base():
-    return render_template('about.html')
+    return render_template('about.html', year_on_site=year_on_site())
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html', year_on_site=year_on_site())
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html', year_on_site=year_on_site())
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template("error_pages/404.html"), 404
+    return render_template("error_pages/404.html", year_on_site=year_on_site()), 404
+
+
+def year_on_site():
+    date_now = datetime.datetime.now()
+    return date_now.year
+
